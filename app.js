9371258407a424f64503f9a319f361795416ff91
Fazzacoot <farongottlieb@gmail.com>
2020-01-13 21:16:57 +0200

    S2 M19 - Woring with Enums

--- app.js
+++ app.js
@@ -1,10 +1,35 @@
 //You can explisitly tell TS what values the object has
 //const person: { name: string; age: number }
 //TS infers the values here
+// const person = {
+//   name: "Faron",
+//   age: 24,
+//   hobbies: ["Video Games", "Coding"],
+//   role: [2, "Author"]
+// };
+//Becuase of Tuple we need to specify types otherwise we can not know what the tuples values need to be
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: "Faron",
+//   age: 24,
+//   hobbies: ["Video Games", "Coding"],
+//   role: [2, "Author"]
+// };
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
 var person = {
     name: "Faron",
     age: 24,
-    hobbies: ["Video Games", "Coding"]
+    hobbies: ["Video Games", "Coding"],
+    role: Role.ADMIN
 };
 var favouriteActivities;
 favouriteActivities = ["Vide Games"];
@@ -12,4 +37,7 @@ console.log(person);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby.toUpperCase());
+}
+if (person.role === Role.ADMIN) {
+    console.log('is admin');
 }
--- app.ts
+++ app.ts
@@ -10,18 +10,32 @@
 // };
 
 //Becuase of Tuple we need to specify types otherwise we can not know what the tuples values need to be
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string];
-} = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: "Faron",
+//   age: 24,
+//   hobbies: ["Video Games", "Coding"],
+//   role: [2, "Author"]
+// };
+
+//Can assigne values to the enum values eg: ADMIN = 100 
+//defualt starts at 0 and increments by 1
+enum Role {
+  ADMIN,
+  READ_ONLY,
+  AUTHOR
+}
+
+const person = {
   name: "Faron",
   age: 24,
   hobbies: ["Video Games", "Coding"],
-  role: [2, "Author"]
+  role: Role.ADMIN
 };
-
 
 let favouriteActivities: string[];
 favouriteActivities = ["Vide Games"];
@@ -30,4 +44,8 @@ console.log(person);
 
 for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase());
+}
+
+if(person.role === Role.ADMIN){
+  console.log('is admin');
 }
