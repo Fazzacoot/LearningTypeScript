ba9f507c70c385116115c5b88d3676a2a1eab515
Fazzacoot <farongottlieb@gmail.com>
2020-01-13 22:34:16 +0200

    S2 M21 - Union Types

--- app.js
+++ app.js
@@ -1,43 +1,14 @@
-//You can explisitly tell TS what values the object has
-//const person: { name: string; age: number }
-//TS infers the values here
-// const person = {
-//   name: "Faron",
-//   age: 24,
-//   hobbies: ["Video Games", "Coding"],
-//   role: [2, "Author"]
-// };
-//Becuase of Tuple we need to specify types otherwise we can not know what the tuples values need to be
-// const person: {
-//   name: string;
-//   age: number;
-//   hobbies: string[];
-//   role: [number, string];
-// } = {
-//   name: "Faron",
-//   age: 24,
-//   hobbies: ["Video Games", "Coding"],
-//   role: [2, "Author"]
-// };
-var Role;
-(function (Role) {
-    Role[Role["ADMIN"] = 0] = "ADMIN";
-    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
-    Role[Role["AUTHOR"] = 2] = "AUTHOR";
-})(Role || (Role = {}));
-var person = {
-    name: "Faron",
-    age: 24,
-    hobbies: ["Video Games", "Coding"],
-    role: Role.ADMIN
-};
-var favouriteActivities;
-favouriteActivities = ["Vide Games"];
-console.log(person);
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    console.log(hobby.toUpperCase());
+function combine(input1, input2) {
+    var result;
+    if (typeof input1 === 'number' && typeof input2 === 'number') {
+        result = input1 + input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
 }
-if (person.role === Role.ADMIN) {
-    console.log('is admin');
-}
+var combinedAges = combine(23, 10);
+console.log(combinedAges);
+var combinedNames = combine('Faron', 'Gottlieb');
+console.log(combinedNames);
--- app.ts
+++ app.ts
@@ -1,51 +1,16 @@
-//You can explisitly tell TS what values the object has
-//const person: { name: string; age: number }
-
-//TS infers the values here
-// const person = {
-//   name: "Faron",
-//   age: 24,
-//   hobbies: ["Video Games", "Coding"],
-//   role: [2, "Author"]
-// };
-
-//Becuase of Tuple we need to specify types otherwise we can not know what the tuples values need to be
-// const person: {
-//   name: string;
-//   age: number;
-//   hobbies: string[];
-//   role: [number, string];
-// } = {
-//   name: "Faron",
-//   age: 24,
-//   hobbies: ["Video Games", "Coding"],
-//   role: [2, "Author"]
-// };
-
-//Can assigne values to the enum values eg: ADMIN = 100 
-//defualt starts at 0 and increments by 1
-enum Role {
-  ADMIN,
-  READ_ONLY,
-  AUTHOR
+function combine(input1: number | string, input2:  number | string) {
+  let result;
+  if(typeof input1 === 'number' && typeof input2 === 'number'){
+     result = input1 + input2;
+  }else{
+    result = input1.toString() + input2.toString();
+  }
+  
+  return result;
 }
 
-const person = {
-  name: "Faron",
-  age: 24,
-  hobbies: ["Video Games", "Coding"],
-  role: Role.ADMIN
-};
+const combinedAges = combine(23,10);
+console.log(combinedAges)
 
-let favouriteActivities: string[];
-favouriteActivities = ["Vide Games"];
-
-console.log(person);
-
-for (const hobby of person.hobbies) {
-  console.log(hobby.toUpperCase());
-}
-
-if(person.role === Role.ADMIN){
-  console.log('is admin');
-}
+const combinedNames = combine('Faron','Gottlieb');
+console.log(combinedNames)
