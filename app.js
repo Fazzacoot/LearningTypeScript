e4f48cdaaa6f7f78cc208c629dee110397431966
Fazzacoot <farongottlieb@gmail.com>
2020-01-14 21:12:08 +0200

    S2 M26 - Function as Types

--- app.js
+++ app.js
@@ -3,7 +3,12 @@
 function add(n1, n2) {
     return n1 + n2;
 }
+//type of void 
 function printAnswer(num) {
-    console.log('Result:' + num);
+    console.log("Result:" + num);
 }
 printAnswer(add(10, 23));
+var combineValues;
+combineValues = add;
+console.log(combineValues(10, 10));
+//let someUndefinedValue: undefined;
--- app.ts
+++ app.ts
@@ -4,11 +4,18 @@ function add(n1: number, n2: number) {
   return n1 + n2;
 }
 
-//type of void
+//type of void 
 function printAnswer(num: number) {
   console.log("Result:" + num);
 }
 
 printAnswer(add(10, 23));
+
+//accept a function that takes 2 number perameters and return a number
+let combineValues: (x: number, y: number) => number;
+
+combineValues = add;
+
+console.log(combineValues(10,10));
 
 //let someUndefinedValue: undefined;
