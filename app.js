34c5320e3e5530a5251d499e6790d2cbb1026f1a
Fazzacoot <farongottlieb@gmail.com>
2020-01-14 21:17:46 +0200

    S2 M27 - Function Types & Callbacks

--- app.js
+++ app.js
@@ -3,12 +3,21 @@
 function add(n1, n2) {
     return n1 + n2;
 }
-//type of void 
+//type of void
 function printAnswer(num) {
     console.log("Result:" + num);
 }
+function addAndHandle(n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
+}
 printAnswer(add(10, 23));
+//Function Types
+//accept a function that takes 2 number perameters and return a number
 var combineValues;
 combineValues = add;
 console.log(combineValues(10, 10));
 //let someUndefinedValue: undefined;
+addAndHandle(6, 3, function (result) {
+    console.log(result);
+});
--- app.ts
+++ app.ts
@@ -4,18 +4,28 @@ function add(n1: number, n2: number) {
   return n1 + n2;
 }
 
-//type of void 
+//type of void
 function printAnswer(num: number) {
   console.log("Result:" + num);
 }
 
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+  cb(result);
+}
+
 printAnswer(add(10, 23));
 
+//Function Types
 //accept a function that takes 2 number perameters and return a number
 let combineValues: (x: number, y: number) => number;
 
 combineValues = add;
 
-console.log(combineValues(10,10));
+console.log(combineValues(10, 10));
 
 //let someUndefinedValue: undefined;
+
+addAndHandle(6,3,(result)=>{
+  console.log(result)
+})
