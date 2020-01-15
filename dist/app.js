b6c55cd5182c8a43188f62823921e9e61bef08b0
Fazzacoot <farongottlieb@gmail.com>
2020-01-15 21:44:09 +0200

    S5 M60 - Constructon and this

--- dist/app.js
+++ dist/app.js
@@ -3,7 +3,13 @@ class Department {
     constructor(n) {
         this.name = n;
     }
+    describe() {
+        console.log("Department: " + this.name);
+    }
 }
 const development = new Department("Development");
 console.log(development);
+development.describe();
+const developmentCopy = { name: 'HR', describe: development.describe };
+developmentCopy.describe();
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,20 @@ class Department {
     //sets name to the value passed in and returns in to our object
     this.name = n;
   }
+
+  //this: Department tells us that descripe expecs and instance of the class Department
+  describe(this: Department) {
+    //this refers to classes properties
+    console.log("Department: " + this.name);
+  }
 }
 //Department object
 const development = new Department("Development");
 console.log(development);
+development.describe();
+
+//returns undefined as this is not avaliabe
+// const developmentCopy = {describe: development.describe};
+// developmentCopy.describe();
+const developmentCopy = {name: 'HR', describe: development.describe};
+developmentCopy.describe();
