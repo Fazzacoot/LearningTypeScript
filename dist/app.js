7128c34383d87ac01221b38c85764a4e5dcd2ecc
Fazzacoot <farongottlieb@gmail.com>
2020-01-15 21:07:08 +0200

    S4 Completed

--- dist/app.js
+++ dist/app.js
@@ -1,3 +1,32 @@
 "use strict";
-console.log('Moving on to S4');
+const Name = "Faron Gottlieb";
+let myAge = 24;
+var hasHair = true;
+const add = (a, b = 4) => {
+    return a + b;
+};
+const shortHandAdd = (a, b) => a + b;
+const printOutput = output => console.log(output);
+printOutput(add(12, 34));
+printOutput(add(12));
+const hobbies = ["Video Games", "Reading"];
+const activeHobbies = ["Watching Netflix", ...hobbies];
+activeHobbies.push(...hobbies);
+console.log(activeHobbies);
+const [hobby1, hobby2, ...remainingHobbies] = hobbies;
+console.log(hobby1, hobby2, hobbies);
+const person = {
+    firstName: "Faron",
+    age: 24
+};
+const copiedPerson = Object.assign({}, person);
+const { firstName: userName, age } = person;
+console.log(userName, age);
+const multiAdd = (...numbers) => {
+    return numbers.reduce((result, value) => {
+        return result + value;
+    }, 0);
+};
+const addedNumbers = multiAdd(1, 5, 6, 4, 9, 4.4);
+console.log(addedNumbers);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,66 @@
-console.log('Moving on to S4')
+/*
+All are global scoped
+*/
+//const - value can not be change. has global, functional and block scope
+const Name = "Faron Gottlieb";
+//let - value can be change. has global, functional and block scope
+let myAge = 24;
+//var -  value can be change. has global and functional
+var hasHair = true;
+
+//arrow functions
+//default parameter values can be set in the parameter listing
+//default parameter values need to be at the of the parameter list
+const add = (a: number, b: number = 4) => {
+  return a + b;
+};
+//short hand add
+const shortHandAdd = (a: number, b: number) => a + b;
+//single parameter does not requier () around values but needs to be told what t0 expect
+const printOutput: (a: number | string) => void = output => console.log(output);
+
+printOutput(add(12, 34));
+
+//uses defualt value
+printOutput(add(12));
+
+/*
+... (3 dots) called a spread opperator in this case is used to pull each individual 
+value from the array as individual values
+*/
+const hobbies = ["Video Games", "Reading"];
+const activeHobbies = ["Watching Netflix", ...hobbies];
+
+activeHobbies.push(...hobbies);
+console.log(activeHobbies);
+
+//destructuring an array i.e pulling items out of the array
+//values are coppied into new variabels/constants
+const [hobby1, hobby2, ...remainingHobbies] = hobbies;
+console.log(hobby1, hobby2, hobbies);
+
+const person = {
+  firstName: "Faron",
+  age: 24
+};
+
+//this creates a perfect copt of the person object and not just the pointer that is in memeory
+const copiedPerson = { ...person };
+
+//destructuring an object i.e pulling items out of the array
+//values are coppied into new variabels/constants
+// firstName: userName overiding variable name to userName
+const { firstName: userName, age } = person;
+console.log(userName, age);
+/*
+... (3 dots) indicates a rest parameter in this case
+this meas that multiAdd can accept any number of parameters as a number array
+*/
+const multiAdd = (...numbers: number[]) => {
+  return numbers.reduce((result, value) => {
+    return result + value;
+  }, 0);
+};
+
+const addedNumbers = multiAdd(1, 5, 6, 4, 9, 4.4);
+console.log(addedNumbers);
