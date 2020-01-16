a982b0678b1c0e9f92c6b85116cf47cd2511edad
Fazzacoot <farongottlieb@gmail.com>
2020-01-16 21:03:48 +0200

    S5 M64-66  - inheritance, overiding, protected, getter and setter

--- dist/app.js
+++ dist/app.js
@@ -9,11 +9,49 @@ class Department {
         console.log(`Department (${this.id}): ${this.name}`);
     }
     addEmployee(employee) {
-        this, this.employees.push(employee);
+        this.employees.push(employee);
     }
     showEmployeeInformation() {
         console.log(this.employees.length);
         console.log(this.employees);
+    }
+}
+class HRDepartment extends Department {
+    constructor(id, managers) {
+        super(id, "Human Resources");
+        this.managers = managers;
+    }
+}
+class AccountingDepartment extends Department {
+    constructor(id, reports) {
+        super(id, "Accounting");
+        this.reports = reports;
+        this.lastReport = reports[0];
+    }
+    get mostRecentReport() {
+        if (this.lastReport) {
+            return this.lastReport;
+        }
+        throw new Error("No report found");
+    }
+    set mostRecentReport(value) {
+        if (!value) {
+            throw new Error("No valid value passed");
+        }
+        this.addRepot(value);
+    }
+    addEmployee(name) {
+        if (name === "Faron") {
+            return;
+        }
+        this.employees.push(name);
+    }
+    addRepot(text) {
+        this.reports.push(text);
+        this.lastReport = text;
+    }
+    printRepots() {
+        console.log(this.reports);
     }
 }
 const development = new Department("DEV", "Development");
@@ -22,4 +60,15 @@ development.describe();
 development.addEmployee("Faron");
 development.addEmployee("Dylan");
 development.showEmployeeInformation();
+const hr = new HRDepartment("HR", ["Meryl"]);
+hr.addEmployee("Dean");
+hr.addEmployee("Mark");
+console.log(hr);
+const accounting = new AccountingDepartment("ACC", []);
+accounting.addRepot("This is a new report");
+accounting.mostRecentReport = 'New report';
+accounting.addEmployee("Faron");
+accounting.addEmployee("Dean");
+console.log(accounting);
+console.log(accounting.mostRecentReport);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
-//clss is our blueprint for out object
+//class is our blueprint for out object
 class Department {
+  /*
+  public - accessiable from anywhere
+  private - accessiable from class only
+  protected - accessiable from class and inherited classes
+  */
   //Department properties
   // private readonly id: string;
   // private name: string;
-  private employees: string[] = []; //emplyees are only accesable inside Department as it is private
+  protected employees: string[] = []; //emplyees are only accesable inside Department as it is private
 
   //called when class is called and determines what values are needed when class is called
   /*
@@ -24,7 +29,7 @@ class Department {
   }
 
   addEmployee(employee: string) {
-    this, this.employees.push(employee);
+    this.employees.push(employee);
   }
 
   showEmployeeInformation() {
@@ -32,6 +37,57 @@ class Department {
     console.log(this.employees);
   }
 }
+
+//Inheritance
+class HRDepartment extends Department {
+  managers: string[];
+  constructor(id: string, managers: string[]) {
+    //super calls the base class and requiers all parameters passed to it
+    super(id, "Human Resources");
+    this.managers = managers;
+  }
+}
+
+class AccountingDepartment extends Department {
+  private lastReport: string;
+  //get a value
+  get mostRecentReport() {
+    if (this.lastReport) {
+      return this.lastReport;
+    }
+    throw new Error("No report found");
+  }
+  //set a value
+  set mostRecentReport(value: string){
+    if (!value) {
+      throw new Error("No valid value passed");
+    }
+    this.addRepot(value);
+  }
+
+  constructor(id: string, private reports: string[]) {
+    //super calls the base class and requiers all parameters passed to it
+    super(id, "Accounting");
+    this.lastReport = reports[0];
+  }
+
+  addEmployee(name: string) {
+    if (name === "Faron") {
+      return;
+    }
+    this.employees.push(name);
+  }
+
+  addRepot(text: string) {
+    this.reports.push(text);
+    this.lastReport = text;
+  }
+
+  printRepots() {
+    console.log(this.reports);
+  }
+}
+
 //Department object
 const development = new Department("DEV", "Development");
 console.log(development);
@@ -40,6 +96,24 @@ development.describe();
 development.addEmployee("Faron");
 development.addEmployee("Dylan");
 development.showEmployeeInformation();
+
+//HR Department
+const hr = new HRDepartment("HR", ["Meryl"]);
+hr.addEmployee("Dean");
+hr.addEmployee("Mark");
+console.log(hr);
+
+//Accounting Department
+const accounting = new AccountingDepartment("ACC", []);
+
+
+accounting.addRepot("This is a new report");
+accounting.mostRecentReport = 'New report';
+accounting.addEmployee("Faron");
+accounting.addEmployee("Dean");
+console.log(accounting);
+console.log(accounting.mostRecentReport);
+
 
 //returns undefined as this is not avaliabe
 // const developmentCopy = {describe: development.describe};
