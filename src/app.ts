d3dfd4367f772dffdde449b0ecf9bff0f3b8691d
Fazzacoot <farongottlieb@gmail.com>
2020-01-17 21:05:50 +0200

    S7 M95 - Constraints

--- src/app.ts
+++ src/app.ts
@@ -16,10 +16,16 @@ promise.then(data => {
 
 //Custom Generics
 //T and U are dynamically set types that are inferred when the function is called based on values passed to the func
-function merge<T, U>(objA: T, objB: U) {
+/*
+You can set constraints that your generic types need to be based on
+Using extends you can tell what type the Generic needs to be based on
+We garentee the type that the function will recieve
+*/
+function merge<T extends object, U extends object>(objA: T, objB: U) {
   return Object.assign(objA, objB);
 }
 const mergedObj = merge({ name: "Faron" }, { age: 24 });
+// const mergedObj = merge({ name: "Faron" }, 30);
 console.log(mergedObj);
 console.log(mergedObj.name);
 //Custom Generics
