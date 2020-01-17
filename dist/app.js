86bfe6786d45a07f74b232537461b408885e0396
Fazzacoot <farongottlieb@gmail.com>
2020-01-17 17:33:51 +0200

    S6 M83-84 - type guards, discriminated unions

--- dist/app.js
+++ dist/app.js
@@ -1,7 +1,59 @@
 "use strict";
 const e1 = {
     name: "Faron",
-    privilages: ["db-admin"],
+    privileges: ["db-admin"],
     startDate: new Date()
 };
+function add(a, b) {
+    if (typeof a === "string" || typeof b === "string") {
+        return a.toString() + b.toString();
+    }
+    return a + b;
+}
+function showEmployeeInfo(emp) {
+    console.log(`Name ${emp.name}`);
+    if ("privileges" in emp) {
+        console.log(`Privleges: ${emp.privileges}`);
+    }
+    if ("startDate" in emp) {
+        console.log(`Start Date: ${emp.startDate}`);
+    }
+}
+showEmployeeInfo(e1);
+class Car {
+    drive() {
+        console.log("Driving");
+    }
+}
+class Truck {
+    drive() {
+        console.log("Driving Truck");
+    }
+    loadCargo(amount) {
+        console.log(`Loading Cargo: ${amount}`);
+    }
+}
+const v1 = new Car();
+const v2 = new Truck();
+function useVheicle(vehicle) {
+    vehicle.drive();
+    if (vehicle instanceof Truck) {
+        vehicle.loadCargo(2345);
+    }
+}
+useVheicle(v1);
+useVheicle(v2);
+function moveAnimal(animal) {
+    let speed;
+    switch (animal.type) {
+        case 'bird':
+            speed = animal.airSpeed;
+            break;
+        case 'dog':
+            speed = animal.landSpeed;
+            break;
+    }
+    console.log(`Speed ${speed}`);
+}
+moveAnimal({ type: 'bird', airSpeed: 12 });
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
-//intersection types
+//intersection types start
 //combied 2 or more types together
 //can be done with custom types or generic types
 type Admin = {
   name: string;
-  privilages: string[];
+  privileges: string[];
 };
 
 type Employee = {
@@ -15,7 +15,7 @@ type EvelvatedEmployee = Admin & Employee;
 
 const e1: EvelvatedEmployee = {
   name: "Faron",
-  privilages: ["db-admin"],
+  privileges: ["db-admin"],
   startDate: new Date()
 };
 
@@ -23,4 +23,95 @@ type Combinable = string | number;
 type Numberic = number | boolean;
 
 type Universal = Combinable & Numberic;
-//intersection types
+//intersection types end
+
+//type guards start
+function add(a: Combinable, b: Combinable) {
+  //values in if == type guard. explicitly checks if 1+ values is of type string
+  //typeof is checked at runtime
+  if (typeof a === "string" || typeof b === "string") {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
+
+type UnknownEmployee = Employee | Admin;
+
+function showEmployeeInfo(emp: UnknownEmployee) {
+  console.log(`Name ${emp.name}`);
+  //js that allows to to check if the property is in the object
+  if ("privileges" in emp) {
+    console.log(`Privleges: ${emp.privileges}`);
+  }
+  if ("startDate" in emp) {
+    console.log(`Start Date: ${emp.startDate}`);
+  }
+}
+
+showEmployeeInfo(e1);
+
+class Car {
+  drive() {
+    console.log("Driving");
+  }
+}
+
+class Truck {
+  drive() {
+    console.log("Driving Truck");
+  }
+
+  loadCargo(amount: number) {
+    console.log(`Loading Cargo: ${amount}`);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVheicle(vehicle: Vehicle) {
+  vehicle.drive();
+  //checks if the vehicle is of the truck class
+  //executes at run time
+  //can us in as well here
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(2345);
+  }
+}
+
+useVheicle(v1);
+useVheicle(v2);
+
+//Discriminated Unions
+//type is a literal type that must equal that value this is the discriminated union value
+interface Bird {
+  type: 'bird',
+  airSpeed:number;
+}
+
+interface Dog{
+  type: 'dog',
+  landSpeed: number
+}
+
+type Animal = Bird | Dog;
+
+function moveAnimal(animal: Animal){
+  let speed;
+  switch(animal.type){
+    case 'bird':
+      speed = animal.airSpeed;
+      break;
+    case 'dog':
+      speed = animal.landSpeed; 
+    break; 
+  }
+  console.log(`Speed ${speed}` )
+}
+
+moveAnimal({type:'bird', airSpeed: 12})
+//Discriminated Unions
+
+//type guards ends
