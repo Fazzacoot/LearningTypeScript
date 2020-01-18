b0eef78cb3745db05fd6af909ca457c8ad331c33
Fazzacoot <farongottlieb@gmail.com>
2020-01-18 11:42:18 +0200

    S8 M105 - decorator factories

--- dist/app.js
+++ dist/app.js
@@ -5,18 +5,20 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-function Logger(constructor) {
-    console.log('Logging');
-    console.log(constructor);
+function Logger(logString) {
+    return function (constructor) {
+        console.log(logString);
+        console.log(constructor);
+    };
 }
 let Person = class Person {
     constructor() {
-        this.name = 'Faron';
-        console.log('Created Person');
+        this.name = "Faron";
+        console.log("Created Person");
     }
 };
 Person = __decorate([
-    Logger
+    Logger('Logging Person')
 ], Person);
 const person = new Person();
 console.log(person);
--- src/app.ts
+++ src/app.ts
@@ -5,17 +5,33 @@
 //@function name to call decorator
 
 //Decorators on a class take 1 argument - the constructor of the class
-function Logger(constructor: Function){
-  console.log('Logging');
-  console.log(constructor);
+//Standerd Decorator decleration
+// function Logger(constructor: Function){
+//   console.log('Logging');
+//   console.log(constructor);
+// }
+//Standerd Decorator decleration
+
+//Decorator Factory
+function Logger(logString: string) {
+  return function(constructor: Function) {
+    console.log(logString);
+    console.log(constructor);
+  };
 }
+//Decorator Factory
 
-@Logger
+//Standerd Decorator call
+// @Logger
+//Standerd Decorator call
+//Decorator Factory call
+@Logger('Logging Person') // executes a function that returs a decorator function
+//Decorator Factory call
 class Person {
-  name = 'Faron';
+  name = "Faron";
 
-  constructor(){
-    console.log('Created Person')
+  constructor() {
+    console.log("Created Person");
   }
 }
 
