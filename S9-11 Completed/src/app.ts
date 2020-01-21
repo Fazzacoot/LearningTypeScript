d75fb90418801312f8e761f7056665e82c0eb9ca
Fazzacoot <farongottlieb@gmail.com>
2020-01-21 22:11:16 +0200

    S9-S11 Completed

---
+++ S9-11 Completed/src/app.ts
@@ -0,0 +1,14 @@
+import { ProjectList } from "./components/project-list";
+import { ProjectForm } from "./components/project-form";
+
+//Import/Export - use "module": "es2015"
+//importing namespace - only exisit in the typescript scope
+//tells TS where to find the code but in JS it can find the code
+//set outFile to tell TS to create one file in the tsconfig
+// /<reference path="./components/project-form.ts"/>
+// /<reference path="./components/project-item.ts"/>
+// /<reference path="./components/project-list.ts"/>
+
+new ProjectForm();
+new ProjectList("active");
+new ProjectList("finished");
